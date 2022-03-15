0cfd7d10c7131c1e10ca0f1ef046d92f4a155d16
Dharmendra <dharmendra.y@gmail.com>
2022-03-15 17:22:36 +0530

    Compare Item

--- testCases/test_Compare_New.py
+++ testCases/test_Compare_New.py
@@ -6,10 +6,12 @@ from utilities.readProperties import ReadConfig
 from utilities.customLogger import LogGen
 from utilities import datetimeutilityNew
 
-class Compare_Item():
+class Test_Compare_Item:
 
     baseURL = "https://build49-legacy.cn-qa-stage.catnav.us/result/ll-categories-home-page-created-from-am-cat1testam?plpver=1155"
     logger = LogGen.loggen()
+
+
     def test_CompareItem_1(self, setup):
 
         self.driver = setup
@@ -17,29 +19,30 @@ class Compare_Item():
         time.sleep(2)
         self.driver.maximize_window()
         time.sleep(1)
-        driver.maximize_window()
+        self.driver.maximize_window()
         time.sleep(2)
         #Item 1 Compare click
-
-        driver.execute_script("return document.querySelector('plp-item-results > plp-thumbnail >plp-compareitem').shadowRoot.querySelector('a > span#compareitemText')").click()
+        self.logger.info("******************** Compare Item clicked ********************")
+        self.driver.execute_script("return document.querySelector('plp-item-results > plp-thumbnail >plp-compareitem').shadowRoot.querySelector('a > span#compareitemText')").click()
         time.sleep(2)
-        Compare_Button_Click2 = driver.execute_script("return document.querySelector('plp-item-results > plp-thumbnail:nth-child(2)>plp-compareitem').shadowRoot.querySelector('a > span#compareitemText')")
-        driver.execute_script("arguments[0].click();", Compare_Button_Click2)
+        Compare_Button_Click2 = self.driver.execute_script("return document.querySelector('plp-item-results > plp-thumbnail:nth-child(2)>plp-compareitem').shadowRoot.querySelector('a > span#compareitemText')")
+        self.driver.execute_script("arguments[0].click();", Compare_Button_Click2)
         time.sleep(3)
-
+        #Item 1 Compare click
+        self.logger.info("******************** Compare button clicked ********************")
         #Clear all button click
         # Clearall_Click = driver.execute_script("return document.querySelector('plp-itemsqueue').shadowRoot.querySelector('#btnClearAll')")
         # driver.execute_script("arguments[0].click();", Clearall_Click)
         # time.sleep(1)
 
-        driver.execute_script("return document.querySelector('plp-itemsqueue').shadowRoot.querySelector('#btnCompare')").click()
+        self.driver.execute_script("return document.querySelector('plp-itemsqueue').shadowRoot.querySelector('#btnCompare')").click()
         #Compare_Click = driver.execute_script("return document.querySelector('plp-itemsqueue').shadowRoot.querySelector('#btnCompare')")
 
         #driver.execute_script("arguments[0].click();", Compare_Click)
         time.sleep(1)
 
-        print(driver.execute_script("return document.title;"))
+        print(self.driver.execute_script("return document.title;"))
 
-        print(driver.execute_script("return document.URL"))
+        print(self.driver.execute_script("return document.URL"))
 
-        driver.close()
+        self.driver.close()
